41960c1db9c84a2619f185d6a5162843eef5fe80
Michal Huniewicz <michal.huniewicz.registered@gmail.com>
2011-09-18 11:11:46 +0100

    Added onload event, console box, string format.

---
+++ scripts/photo-warning.js
@@ -0,0 +1,28 @@
+$(function() {	
+	// This will run on windows onload.
+	
+	// Set onmouseover for the image.
+	$('#graphicPhoto01').bind('mouseover', function(event) {
+		log('Whoa!');
+	});
+	
+	// Set style for the console box.
+	$('#console').css('font-size', '10px');
+});
+
+// Log messages to console.
+function log(message) {
+	$('#console').prepend("{0}: {1}<br />".format(new Date(), message));
+}
+
+// "{0}: blah {1}".format(a, b)
+// http://stackoverflow.com/questions/1038746/equivalent-of-string-format-in-jquery
+String.prototype.format = function() {
+    var s = this,
+        i = arguments.length;
+
+    while (i--) {
+        s = s.replace(new RegExp('\\{' + i + '\\}', 'gm'), arguments[i]);
+    }
+    return s;
+};
